bc0d8fcc83ab43498df901f1e4d8fc1cb85177d1
Antônio Narcilio <49988118+AntonioNarcilio@users.noreply.github.com>
2021-10-05 14:21:13 -0300

    📚 study: create api and  config server
    config server with apollo server, graphql and mongodb (lib mongoose)
    
    source: https://youtu.be/J4xep6HHeIo?list=PLPXWI3llyMiK9uw7tfljM2hnQl2qu6CeT

---
+++ src/models/Post.ts
@@ -0,0 +1,23 @@
+import mongoose  from "mongoose";
+
+const Schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },  
+  content: {
+    type: String,
+    required: true
+  }, 
+  author: {
+    type: mongoose.Schema.Types.ObjectId, // gerando hash id
+    ref: 'User', // referencia ao model User
+    require: true,
+  }, 
+  active: {
+    type: Boolean,
+    required: true
+  },
+})
+
+export default mongoose.model('Post', Schema);
